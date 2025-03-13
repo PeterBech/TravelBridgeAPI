60f1114510443e829c3ecb574b52a6cff56ec038
PeterBech <pibm90@gmail.com>
2025-03-13 14:11:49 +0100

    Oprettet TravelBridgeAPI og inkluderet dokumentation

---
+++ Controllers/FlightController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelBridgAPI.DataHandler;
+
+namespace TravelBridgAPI.Controllers
+{
+    public class FlightController : Controller
+    {
+        [HttpGet:($"/SearchFlights/{start}&{stop}")]
+        public async Task<IActionResult> SearchFlights(string start, string stop)
+        {
+            return null;
+        }
+        [HttpGet:($"/SearchLocations/{location}")]
+        public async Task<IActionResult> SearchLocation(string location)
+        {
+            HandleLocations locations = new HandleLocations();
+            return locations.GetLocation(location);
+        }
+    }
+}
---
+++ DataHandler/HandleLocations.cs
@@ -0,0 +1,12 @@
+﻿using TravelBridgAPI.Models.FlightLocations;
+
+namespace TravelBridgAPI.DataHandler
+{
+    public class HandleLocations
+    {
+        public Rootobject GetLocation(string city)
+        {
+            return null;
+        }
+    }
+}
