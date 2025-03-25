7416ebd19e32f1c06ae4bb0435273e54ba7b278a
PeterBech <pibm90@gmail.com>
2025-03-25 14:52:21 +0100

    Merget med FlightDetails

--- obj/Debug/net9.0/TravelBridgAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/TravelBridgAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TravelBridgAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b5ea9aac160adda5b45257cdaed59acd2edf9cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aad192bde31edd72f6121e74ec6f83b9ccd9606")]
 [assembly: System.Reflection.AssemblyProductAttribute("TravelBridgAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TravelBridgAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
